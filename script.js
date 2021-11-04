ae0f21088b212551e417c71fef8596006263711c
amontheape <mcanedotome@gmail.com>
2021-11-04 15:14:29 -0300

    feat: função de preenchimento implementada

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
-let cardNumber, condition;
+let cardNumber = 0;
 
 function getEntry () {
 
 cardNumber = parseInt(prompt("Insira um número de cartas par entre 4 e 14: "));
 
-    while ( !condition ) {
+    while ( true ) {
 
         if ((cardNumber % 2) !== 0){
             cardNumber = parseInt(prompt("Você digitou um número ímpar\n" + "Insira um número de cartas par entre 4 e 14: "));
-            console.log(cardNumber);
         } else if (cardNumber < 4) {
             cardNumber = parseInt(prompt("Você digitou um número par menor que 4\n" + "Insira um número de cartas par entre 4 e 14: "));
-            console.log(cardNumber);
         } else if (cardNumber > 14) {
             cardNumber = parseInt(prompt("Você digitou um número par maior que 14\n" + "Insira um número de cartas par entre 4 e 14: "));
-            console.log(cardNumber);
         } else {
-            condition = true;
-            console.log(cardNumber);
+            break;
         }
 
     }
 
-    // chamar função de preenchimento;
+    cardFiller(cardNumber);
+}
+
+function cardFiller (value) {
+
+    const main = document.querySelector(".fill-in");
+
+    for (let i = 0 ; i < value ; i++ ) {
+
+        main.innerHTML += `
+        
+        <div class="card" onclick="flip()">
+
+            <div class="front-face face">
+              Frente
+            </div>
+
+            <div class="back-face face">
+              Verso
+            </div>
+
+          </div>` ; 
+
+    }  
+
 }
 
 getEntry();
 
-// criar função de preenchimento com base no número de cartas escolhidas;
 
-// criar função flip
+// criar função flip onclick
+
+// criar verificação de match das cartas e win condition
 
 // criar função random generator
