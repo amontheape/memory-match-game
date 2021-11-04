76e3c33b3a23d5966b4eb3867eaf478d4d164e96
amontheape <mcanedotome@gmail.com>
2021-11-04 14:13:08 -0300

    layout implementado

---
+++ script.js
@@ -0,0 +1,10 @@
+const cardNumber = [];
+
+// criar prompt ;
+// criar tratamento de erros pra números ímpares, menores que 4 ou maiores que 14;
+
+// criar função de preenchimento com base no número de cartas escolhidas;
+
+// criar função flip
+
+// criar função random generator
