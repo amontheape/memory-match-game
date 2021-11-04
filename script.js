91599e7f05b5f77a58856b440aef056be617553c
amontheape <mcanedotome@gmail.com>
2021-11-04 14:41:43 -0300

    feat: função de recolher entrada implementada

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const cardNumber = [];
+let cardNumber, condition;
 
-// criar prompt ;
-// criar tratamento de erros pra números ímpares, menores que 4 ou maiores que 14;
+function getEntry () {
+
+cardNumber = parseInt(prompt("Insira um número de cartas par entre 4 e 14: "));
+
+    while ( !condition ) {
+
+        if ((cardNumber % 2) !== 0){
+            cardNumber = parseInt(prompt("Você digitou um número ímpar\n" + "Insira um número de cartas par entre 4 e 14: "));
+            console.log(cardNumber);
+        } else if (cardNumber < 4) {
+            cardNumber = parseInt(prompt("Você digitou um número par menor que 4\n" + "Insira um número de cartas par entre 4 e 14: "));
+            console.log(cardNumber);
+        } else if (cardNumber > 14) {
+            cardNumber = parseInt(prompt("Você digitou um número par maior que 14\n" + "Insira um número de cartas par entre 4 e 14: "));
+            console.log(cardNumber);
+        } else {
+            condition = true;
+            console.log(cardNumber);
+        }
+
+    }
+
+    // chamar função de preenchimento;
+}
+
+getEntry();
 
 // criar função de preenchimento com base no número de cartas escolhidas;
 
